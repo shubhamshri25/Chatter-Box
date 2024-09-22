5699dbbca9a5474463deb86fe5b3b260d8dbdc93
shubham <shubhshri25gmail.com>
2024-09-22 14:54:35 +0530

    setup mongodb connection

--- Server/connection.js
+++ Server/connection.js
@@ -1,3 +1,13 @@
-const mongoose = require('mongoose');
-const connectionString = process.env.
+const mongoose = require("mongoose");
+const connectionString = process.env.MONGO_DB_URI;
 
+const connectDb = async () => {
+  try {
+    await mongoose.connect(connectionString);
+    console.log("MongoDb connected ");
+  } catch (error) {
+    console.log("MongoDb connection error :", error);
+  }
+};
+
+module.exports = connectDb;
--- Server/index.js
+++ Server/index.js
@@ -3,10 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const connectDb = require("./connection");
 
 app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("Hello from chat-app backend"));
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+connectDb().then(() => {
+  app.listen(port, () => console.log(`Listening on port ${port}!`));
+});
